575b3463b84eb0d517646baaf041e5e7e94e557d
Utopia Lab <89520@fhss.sjp.ac.lk>
2023-03-20 22:33:23 +0530

    setup tailwindcss

--- app/routes/sample-route.jsx
+++ app/routes/sample-route.jsx
@@ -1,5 +1,15 @@
 // any file in routes folder act as a new route
+import { Link } from "@remix-run/react";
 
 export default function SampleRoute() {
-  return <h1>Sample Route Page</h1>;
+  return (
+    <>
+      <h1>Sample Route Page</h1>
+      {/* traditional way to add a link */}
+      <a href="/sample-route">Sample Route - Refreshing Page</a>
+      <br />
+      {/* new way to add a link */}
+      <Link to="/sample-route">Sample Route - No Refresh</Link>
+    </>
+  );
 }
